61c78e6b7f8ad9bd0e3cf3965accc7e142b98aea
Allen <apalacios1994@gmail.com>
2021-04-06 22:57:18 -0500

    missing two

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
-// Write your code here
+function multiply(num1, num2){
+    return num1 * num2;
+}
+let num1 = 31;
+let num2 = 2;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+function random(random) {
+    Number.isInteger(random);
+    return random;
+}
+
+
+function mod(num3, num4){
+    let mod1 = num3 % num4;
+    return mod1;
+}
+let num3 = 10;
+let num4 = 4;
+
+let max = Math.max(1, 5, 6, 7, 20);
+
+function maxTest(max){
+    return max;
+}
