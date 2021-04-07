ebf559e64a5e42962deed8aaf0823f0a007fe8d2
Allen <apalacios1994@gmail.com>
2021-04-07 08:56:28 -0500

    commit tree 305f8d0fdbfc8050956c2de87fafb02e308cbb8b

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-let num1 = 31;
-let num2 = 2;
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
 
 
 let random = Math.floor(Math.random() * 10) + 1;
@@ -14,12 +11,10 @@ function random(random) {
 }
 
 
-function mod(num3, num4){
-    let mod1 = num3 % num4;
-    return mod1;
-}
-let num3 = 10;
-let num4 = 4;
+const num3 = 18;
+const num4 = 7;
+let mod = num3 % num4;
+
 
 let max = Math.max(1, 5, 6, 7, 20);
 
